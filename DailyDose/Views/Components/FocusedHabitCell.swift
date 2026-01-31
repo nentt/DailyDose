f37a1f8872f7074a2177789abbe6f9ab31367aea
Audrey Penninckx <audreypkx@gmail.com>
2026-01-31 17:16:57 +0100

    [Feature] FocusedHabitCell & MainView
    Responsive grid with GeometryReader

---
+++ DailyDose/Views/Components/FocusedHabitCell.swift
@@ -0,0 +1,97 @@
+//
+//  FocusedHabitCell.swift
+//  DailyDose
+//
+//  Created by Audrey Penninckx on 25/01/2026.
+//
+
+import SwiftUI
+
+struct FocusedHabitCell: View {
+    let habit: Habit
+    let geoWidth: Double
+    
+    var body: some View {
+        VStack {
+            
+                switch habit.image {
+                case.asset(let name):
+                    Image(name)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .overlay(
+                            LinearGradient(
+                                colors: [
+                                    Color.gray,
+                                    Color.black.opacity(0.0)
+                                ],
+                                startPoint: .top,
+                                endPoint: .center
+                            )
+                        )
+                        .overlay(
+                            LinearGradient(
+                                colors: [
+                                    Color.gray,
+                                    Color.black.opacity(0.0)
+                                ],
+                                startPoint: .bottom,
+                                endPoint: .center
+                            )
+                        )
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                case.user(let uiImage):
+                    Image(uiImage: uiImage)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(height: 410)
+                        .clipShape(RoundedRectangle(cornerRadius: 45))
+                case.none:
+                    Image(systemName: "photo")
+                        .frame(width: 80, height: 80)
+                        .foregroundStyle(.blackCopy.opacity(0.4))
+                }
+            
+        }
+        .frame(height: 410)
+        .frame(width: geoWidth / 2 - 4)
+        .overlay(alignment: .topLeading){
+            Text("KEEP TRACKING")
+                .font(.custom("Syne-SemiBold", size: 14))
+                .foregroundStyle(.mauveBackground)
+                .frame(width: 100)
+                .multilineTextAlignment(.leading)
+                .padding(.top, 10)
+                .padding(.leading, 5)
+            
+        }
+        .overlay(alignment: .topTrailing){
+            Image(systemName: "arrow.up.right")
+                .font(.system(size: 25))
+                .foregroundColor(.mauveBackground)
+                .padding(.top, 10)
+                .padding(.trailing, 10)
+        }
+        .overlay(alignment: .bottomLeading) {
+            VStack(alignment: .leading) {
+                Text(habit.title.firstUppercased)
+                    .font(.custom("Syne-Bold", size: 33))
+                    .foregroundColor(.yellowButton)
+                    .multilineTextAlignment(.leading)
+                    .padding(.leading, 10)
+                
+                Text(habit.periodicity.description)
+                    .font(.custom("Syne-Regular", size: 14))
+                    .foregroundColor(.yellowButton)
+                    .multilineTextAlignment(.leading) 
+                    .padding(.leading, 10)
+                    .padding(.bottom, 20)
+            }
+        }
+    }
+}
+
+#Preview {
+    FocusedHabitCell(habit: Habit(title: "Drink water", progress: 6, objective: 2, periodicity: .weekly(.hours(2)), unit: .hours(2), image: .asset("drink water")), geoWidth: 700)
+}
+
--- DailyDose/Views/Components/HabitCell.swift
+++ DailyDose/Views/Components/HabitCell.swift
@@ -9,66 +9,153 @@ import SwiftUI
 
 struct HabitCell: View {
     let habit: Habit
+    let isSquare: Bool
     
     var body: some View {
-        VStack(spacing: 12) {
+        //        VStack(spacing: 12) {
+        //            ZStack {
+        //                switch habit.image {
+        //                case.asset(let name):
+        //                    Image(name)
+        //                        .resizable()
+        //                        .scaledToFill()
+        //                        .frame(width: 350, height: 350)
+        //                        .clipShape(RoundedRectangle(cornerRadius: 45))
+        //                case.user(let uiImage):
+        //                    Image(uiImage: uiImage)
+        //                        .resizable()
+        //                        .scaledToFill()
+        //                        .frame(width: 80, height: 80)
+        //                        .clipShape(RoundedRectangle(cornerRadius: 45))
+        //                case.none:
+        //                    Image(systemName: "photo")
+        //                        .frame(width: 80, height: 80)
+        //                        .foregroundStyle(.blackCopy.opacity(0.4))
+        //                }
+        //                VStack {
+        //                        HStack {
+        //                            Spacer()
+        //
+        //                            Image(systemName: "arrow.up.right")
+        //                                    .font(.system(size: 25))
+        //                                    .foregroundColor(.blackCopy)
+        //                                    .frame(width: 50, height: 50)
+        //                                    .background(Circle().fill(Color.yellowButton))
+        //                                    .padding(20)
+        //                        }
+        //                        Spacer()
+        //                    }
+        //            }
+        //            HStack {
+        //                VStack(alignment: .leading) {
+        //                    Text(habit.title.firstUppercased)
+        //                        .font(.custom("Syne-SemiBold", size: 19))
+        //
+        //                    Text(habit.periodicity.description)
+        //                        .font(.custom("Syne-Regular", size: 14))
+        //                        .foregroundColor(.blackCopy.opacity(0.8))
+        //                }
+        //                Spacer()
+        //                Image(systemName: "circle")
+        //                    .font(.system(size: 35, weight: .thin))
+        //                    .multilineTextAlignment(.leading)
+        //            }
+        //            .padding(.horizontal, 20)
+        //            .padding(.bottom, 30)
+        //        }
+        //        .padding(.top, 5)
+        //        .foregroundStyle(.blackCopy)
+        //        .padding(10)
+        //        .frame(maxWidth: .infinity)
+        //        .frame(height: 450)
+        //        .background(.blackCopy.opacity(0.05))
+        //        .cornerRadius(50)
+        //
+        HStack {
             ZStack {
-                switch habit.image {
-                case.asset(let name):
-                    Image(name)
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 350, height: 350)
-                        .clipShape(RoundedRectangle(cornerRadius: 45))
-                case.user(let uiImage):
-                    Image(uiImage: uiImage)
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 80, height: 80)
-                        .clipShape(RoundedRectangle(cornerRadius: 45))
-                case.none:
-                    Image(systemName: "photo")
-                        .frame(width: 80, height: 80)
-                        .foregroundStyle(.blackCopy.opacity(0.4))
-                }
-                VStack {
+                if isSquare {
+                    ZStack {
+                        switch habit.image {
+                        case.asset(let name):
+                            Image(name)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(maxWidth: .infinity)
+                                .frame(height: 200)
+                                .clipShape(RoundedRectangle(cornerRadius: 20))
+                        case.user(let uiImage):
+                            Image(uiImage: uiImage)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 80, height: 80)
+                                .clipShape(RoundedRectangle(cornerRadius: 45))
+                        case.none:
+                            Image(systemName: "photo")
+                                .frame(width: 80, height: 80)
+                                .foregroundStyle(.blackCopy.opacity(0.4))
+                        }
                         HStack {
                             Spacer()
-                        
-                            Image(systemName: "arrow.up.right")
+
+                                Image(systemName: "arrow.up.right")
                                     .font(.system(size: 25))
                                     .foregroundColor(.blackCopy)
                                     .frame(width: 50, height: 50)
                                     .background(Circle().fill(Color.yellowButton))
                                     .padding(20)
+                                    .padding(.bottom, 110)
+                            
                         }
-                        Spacer()
                     }
-            }
-            HStack {
-                VStack(alignment: .leading) {
-                    Text(habit.title.firstUppercased)
-                        .font(.custom("Syne-SemiBold", size: 19))
                     
-                    Text(habit.periodicity.description)
-                        .font(.custom("Syne-Regular", size: 14))
-                        .foregroundColor(.blackCopy.opacity(0.8))
+                } else {
+                    ZStack {
+                        switch habit.image {
+                        case.asset(let name):
+                            Image(name)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(maxWidth: .infinity)
+                                .frame(height: 150)
+                                .clipShape(RoundedRectangle(cornerRadius: 20))
+                        case.user(let uiImage):
+                            Image(uiImage: uiImage)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 80, height: 80)
+                                .clipShape(RoundedRectangle(cornerRadius: 45))
+                        case.none:
+                            Image(systemName: "photo")
+                                .frame(width: 80, height: 80)
+                                .foregroundStyle(.blackCopy.opacity(0.4))
+                        }
+                        HStack {
+                            Spacer()
+
+                                Image(systemName: "arrow.up.right")
+                                    .font(.system(size: 25))
+                                    .foregroundColor(.blackCopy)
+                                    .frame(width: 50, height: 50)
+                                    .background(Circle().fill(Color.yellowButton))
+                                    .padding(20)
+                                    .padding(.bottom, 60)
+                            
+                        }
+                    }
                 }
-                Spacer()
-                Image(systemName: "circle")
-                    .font(.system(size: 35, weight: .thin))
-                    .multilineTextAlignment(.leading)
+                
+                
+                    
             }
-            .padding(.horizontal, 20)
-            .padding(.bottom, 30)
         }
-        .padding(.top, 5)
-        .foregroundStyle(.blackCopy)
-        .padding(10)
-        .frame(maxWidth: .infinity)
-        .frame(height: 450)
-        .background(.blackCopy.opacity(0.05))
-        .cornerRadius(50)
+    }
+}
+
+func shareForHabit(_ index: Int) -> Bool {
+    if index % 2 == 0 {
+        return true
+    } else {
+        return false
     }
 }
 
@@ -79,8 +166,8 @@ extension String {
 }
 #Preview {
     VStack {
-        HabitCell(habit: Habit(title: "pilates", progress: 6, objective: 2, periodicity: .weekly(.hours(2)), unit: .hours(2), image: .asset("run")))
+        HabitCell(habit: Habit(title: "pilates", progress: 6, objective: 2, periodicity: .weekly(.hours(2)), unit: .hours(2), image: .asset("run")), isSquare: true)
     }
     .padding()
-
+    
 }
